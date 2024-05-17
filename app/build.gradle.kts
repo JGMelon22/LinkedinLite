6e231fe56f25a57251cfeb1d88556cf64ee0592f
JGMelon22 <jgvasconcellos22@gmail.com>
2024-05-17 12:38:06 -0300

    chore: bumping dependencies and AGP to 8.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -49,7 +49,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
