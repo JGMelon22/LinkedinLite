30811b284aa0cf304be8cbf7bcb57985f41e053a
JGMelon22 <jgvasconcellos22@gmail.com>
2024-07-27 10:01:38 -0300

    feat: bump AGP to 8.5.1 and simplifying JS call

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
